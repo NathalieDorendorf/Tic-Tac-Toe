8f4ec1342519f8d91a77d89dc6a0aae180105316
Nathalie Dorendorf <dorendorfnathalie@gmail.com>
2024-06-14 19:49:40 +0200

    3rd try with prompt engeneering

--- script.js
+++ script.js
@@ -18,121 +18,27 @@ function init() {
 
 function render() {
     let container = document.getElementById('container');
-    let html = '<table>';
-
+    let tableHTML = '<table>';
+    
     for (let i = 0; i < 3; i++) {
-        html += '<tr>';
+        tableHTML += '<tr>';
         for (let j = 0; j < 3; j++) {
-            let index = i * 3 + j;
-            let content = '';
-            if (fields[index] === 'circle') {
-                content = '<span class="circle">O</span>';
-            } else if (fields[index] === 'cross') {
-                content = '<span class="cross">X</span>';
+            let fieldIndex = i * 3 + j;
+            let fieldClass = fields[fieldIndex];
+            let fieldContent = '';
+            
+            if (fieldClass === 'circle') {
+                fieldContent = 'O';
+            } else if (fieldClass === 'cross') {
+                fieldContent = 'X';
             }
-            html += `<td>${content}</td>`;
+            
+            tableHTML += `<td class="${fieldClass || ''}">${fieldContent}</td>`;
         }
-        html += '</tr>';
+        tableHTML += '</tr>';
     }
-
-    html += '</table>';
-    container.innerHTML = html;
+    
+    tableHTML += '</table>';
+    container.innerHTML = tableHTML;
 }
 
-
-
-// let X_CLASS = 'x';
-// let O_CLASS = 'o';
-// let WINNING_COMBINATIONS = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6]
-// ];
-
-// let cells = [];
-// for (let i = 0; i < 9; i++) {
-//     cells.push(document.getElementById('cell' + i));
-// }
-// let board = document.getElementById('board');
-// let winningMessageElement = document.getElementById('winning-message');
-// let winningMessageTextElement = document.getElementById('winning-message-text');
-// let restartButton = document.getElementById('restartButton');
-// let oTurn;
-
-// startGame();
-
-// restartButton.addEventListener('click', startGame);
-
-// function startGame() {
-//     oTurn = false;
-//     cells.forEach(cell => {
-//         if (cell) {
-//             cell.classList.remove(X_CLASS);
-//             cell.classList.remove(O_CLASS);
-//             cell.removeEventListener('click', handleClick);
-//             cell.addEventListener('click', handleClick, { once: true });
-//         }
-//     });
-//     setBoardHoverClass();
-//     winningMessageElement.classList.remove('show');
-// }
-
-// function handleClick(e) {
-//     let cell = e.target;
-//     let currentClass = oTurn ? O_CLASS : X_CLASS;
-//     placeMark(cell, currentClass);
-//     if (checkWin(currentClass)) {
-//         endGame(false);
-//     } else if (isDraw()) {
-//         endGame(true);
-//     } else {
-//         swapTurns();
-//         setBoardHoverClass();
-//     }
-// }
-
-// function endGame(draw) {
-//     if (draw) {
-//         winningMessageTextElement.innerText = 'Draw!';
-//     } else {
-//         winningMessageTextElement.innerText = `${oTurn ? "O's" : "X's"} Wins!`;
-//     }
-//     winningMessageElement.classList.add('show');
-// }
-
-// function isDraw() {
-//     return cells.every(cell => {
-//         return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
-//     });
-// }
-
-// function placeMark(cell, currentClass) {
-//     cell.classList.add(currentClass);
-// }
-
-// function swapTurns() {
-//     oTurn = !oTurn;
-// }
-
-// function setBoardHoverClass() {
-//     board.classList.remove(X_CLASS);
-//     board.classList.remove(O_CLASS);
-//     if (oTurn) {
-//         board.classList.add(O_CLASS);
-//     } else {
-//         board.classList.add(X_CLASS);
-//     }
-// }
-
-// function checkWin(currentClass) {
-//     return WINNING_COMBINATIONS.some(combination => {
-//         return combination.every(index => {
-//             return cells[index].classList.contains(currentClass);
-//         });
-//     });
-// }
